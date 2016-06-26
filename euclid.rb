aa0a13d2c0551ba09603d5b59eca9418fc12345c
d-m-u <drewuhlmann@gmail.com>
2016-06-26 17:31:05 -0400

    primes on page 16

--- euclid.rb
+++ euclid.rb
@@ -22,35 +22,57 @@ def gcd3 (v,u,w)
 end
 
 def toBinary (x)
-  if (x>0) 
-    return x.to_s if [0,1].include?(x)
+  if (x>1)
     toBinary(x/2) + (x%2).to_s
+  elsif x==1
+    return x.to_s
   end
-end 
+end
 
-puts gcd(1073741823,1073741823) 
+#puts gcd(1073741823,1073741823)
 # largest representable as int in ruby fixnum
-# courtesy of SO 
+# courtesy of SO
 # http://stackoverflow.com/questions/535721/ruby-max-integer
-puts toBinary(3)
 
-  #pascal
-  # program euclid(input.output);
-  # var x.y: integer;
-  # function gcd(u.v: integer):integer;
-  #   var t: integer;
-  #   begin
-  #   repeat
-  #     if u<v then
-  #       begin t:=u; u:=v; v:=t end;
-  #     u:=u-v
-  #   until u=0;
-  #   gcd:=v
-  #   end;
-  # begin
-  # while not eof do
-  #   begin
-  #   readln(x.y);
-  #   if (x>y) and (y>0) then writeln(x,y.gcd(x.y))
-  #   end;
-  # end.
+#pascal
+# program euclid(input.output);
+# var x.y: integer;
+# function gcd(u.v: integer):integer;
+#   var t: integer;
+#   begin
+#   repeat
+#     if u<v then
+#       begin t:=u; u:=v; v:=t end;
+#     u:=u-v
+#   until u=0;
+#   gcd:=v
+#   end;
+# begin
+# while not eof do
+#   begin
+#   readln(x.y);
+#   if (x>y) and (y>0) then writeln(x,y.gcd(x.y))
+#   end;
+# end.
+
+#######################################
+
+# for each i, a[i] true if prime
+# for each i, setting array element corresponding to each multiple of i to false
+# then loop through again, printing them out
+def primes
+  arr=Array.new
+  arr[1]=false
+  (2..1000).each {|i| arr[i]=true}
+  (2..1000).each {|i| (i/2).floor
+    (2..1000).each {|j| (j/i).floor
+      arr[i*j] = false
+  }}
+  for i in 1..1000
+    if arr[i] == true
+      puts i
+    end
+  end
+end
+
+primes()
