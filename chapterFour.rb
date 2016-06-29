e2df7687929a73329489f1f3ee290a7c63301a6d
d-m-u <drewuhlmann@gmail.com>
2016-06-28 23:44:04 -0400

    4.1 and 4.2

---
+++ chapterFour.rb
@@ -0,0 +1,15 @@
+# 4.1 
+# preorder, inorder, postorder, level tree traversal of tree in 4.3 
+# pre -- root, left sub, right sub
+# E O A C P M L T E T E T E
+# in --  left, root, right
+# A C O M P L E T E T R E E
+# post -- left, right, root
+# A C M L P O T E R E E T E
+# level -- top to bottom, left to right
+# E O T P E E A C M L T R E
+
+# 4.2 
+# height of a complete 4-way tree with N nodes
+# height of full binary tree with N internal nodes is about log_2 N
+
