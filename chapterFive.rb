9fa8bc5c6a954c29e032fe9d85b914a432af4261
d-m-u <drewuhlmann@gmail.com>
2016-07-02 20:55:00 -0400

    started recursive josephus from three

--- chapterFive.rb
+++ chapterFive.rb
@@ -5,7 +5,38 @@
 
 def josephus(n,m)
 
+#base case
+N==1
+#everything else calls
+#but I don't know how to keep the placeholder that the original n was in
+n=n-1
+josephus(n,m)
+
 end
+
+
+def josephus(n,m)
+  arr = Array.new
+  order = Array.new
+
+  for i in 1..n
+    arr[i] = i
+  end
+
+  arr.compact!
+
+  for i in 0..n-1
+    arr= arr.rotate(m-1)
+    order << arr[0]
+    arr[0] = nil
+    arr.compact!
+  end
+
+  puts "order: #{order.to_s}"
+end
+
+
+josephus(9,5)
 
 
 # 2.3
